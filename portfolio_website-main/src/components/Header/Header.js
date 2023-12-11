da85bef895ac5ebd872a9633b6922a5625b81879
Nika Berdznishvili <berdznanika12@gmail.com>
2023-12-11 22:14:21 +0400

    Update Header.js

--- portfolio_website-main/src/components/Header/Header.js
+++ portfolio_website-main/src/components/Header/Header.js
@@ -32,13 +32,11 @@ const Header = () =>  (
       </li>        
     </Div2>
       <Div3>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://github.com/scythx0">
           <AiFillGithub size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
-          <AiFillLinkedin size="3rem" />
         </SocialIcons>
-        <SocialIcons href="https://google.com">
+        <SocialIcons href="https://www.instagram.com/berdznishvili.nika/">
           <AiFillInstagram size="3rem"/>
         </SocialIcons>
       </Div3>
